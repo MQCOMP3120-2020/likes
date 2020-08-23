e0aa922a0520c25c3a686521d17f4ddca1e1dd55
Steve Cassidy <steve.cassidy@mq.edu.au>
2020-08-23 13:11:47 +1000

    Add procfile for Heroku deploy

---
+++ Procfile
@@ -0,0 +1,2 @@
+release: npm run build
+web: node server/server.js
--- server/server.js
+++ server/server.js
@@ -73,7 +73,7 @@ app.post('/api/login', async (req, res) => {
 })
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
