34e5ba988176762936bb68af96a95fcab5803617
Steve Cassidy <steve.cassidy@mq.edu.au>
2020-10-27 22:07:47 +1100

    move components to separate folder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, {useState, useEffect} from 'react'
 import './App.css' 
-import List from './List.js'
-import ThingForm from './ThingForm.js'
+import List from './components/List.js'
+import ThingForm from './components/ThingForm.js'
 import likesService from './services/likes'
-import LoginForm from './LoginForm'
+import LoginForm from './components/LoginForm'
 
 const App = () => { 
 
