cba25d0dd3d9ddb8c1d562b3d64a0048784398b7
Steve Cassidy <steve.cassidy@mq.edu.au>
2023-09-21 12:21:16 +1000

    Upgrade React and other packages
    Signed-off-by: Steve Cassidy <steve.cassidy@mq.edu.au>

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css'; 
 import App from './App'; 
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-const refresh = () => {
-  ReactDOM.render(
+const domNode = document.getElementById('root');
+const root = createRoot(domNode);
+
+root.render(
     <React.StrictMode>
       <App />
     </React.StrictMode>,
-    document.getElementById('root')
-  );
-}
-
-refresh()
-
-serviceWorkerRegistration.register()
+);
